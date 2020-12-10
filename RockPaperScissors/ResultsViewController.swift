84d4a8005f2420a65f34786fee46aae9c06f3177
Anthony <sean.a.protho1@gmail.com>
2020-12-10 10:01:06 -0800

    first

---
+++ RockPaperScissors/ResultsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultsViewController.swift
+//  RockPaperScissors
+//
+//  Created by Adriana González Martínez on 11/25/19.
+//  Copyright © 2019 Adriana González Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    var userSelection : GameOption!
+    
+    //TODO: Set up an outlet for the label
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let randomOption = GameOption.init()
+        
+        switch randomOption {
+        case .rock:
+            resultImage.image = UIImage(named: "rock")
+        case .paper:
+            resultImage.image = UIImage(named: "paper")
+        case .scissors:
+            resultImage.image = UIImage(named: "scissors")
+        }
+        
+        //TODO: Depending on the result, show the legend "You won", "You lost", "It's a tie" in the label
+
+    }
+}
